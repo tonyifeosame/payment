ee1c9b1c663e2a8f95f4c15b114fd46ada081da8
tonyifeosame <tonyifeosame@gmail.com>
2025-09-17 22:07:12 +0100

    fixed issues

---
+++ app/Http/Controllers/Categorycontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('categories.index', compact('categories'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255'
+        ]);
+
+        Category::create([
+            'name' => $request->name
+        ]);
+
+        return redirect('/categories')->with('success', 'Category created successfully!');
+    }
+}
