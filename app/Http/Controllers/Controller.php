cfca10c9b0714f6c52e6dcb72c1063b0a190f1fa
provydon <providenceifeosame@gmail.com>
2025-09-17 22:10:10 +0100

    Refactor Controller class to be abstract and remove CategoryController methods

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,27 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\Category;
-
-class CategoryController extends Controller
+abstract class Controller
 {
-    public function index()
-    {
-        $categories = Category::all();
-        return view('categories.index', compact('categories'));
-    }
-
-    public function store(Request $request)
-    {
-        $request->validate([
-            'name' => 'required|string|max:255'
-        ]);
-
-        Category::create([
-            'name' => $request->name
-        ]);
-
-        return redirect('/categories')->with('success', 'Category created successfully!');
-    }
+    //
 }
