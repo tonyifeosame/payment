0ea850f1f4b3588c9d10b1bc00320244a4e637e2
tonyifeosame <tonyifeosame@gmail.com>
2025-11-02 21:36:18 +0100

    changes new

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+
+class TrustProxies extends Middleware
+{
+    public function handle(Request $request, Closure $next)
+    {
+        $this->proxies = '*';
+
+        $this->headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
+
+        return $next($request);
+    }
+}
+
