e630159938cfff856a405a58d7a1dee97f38008a
tonyifeosame <tonyifeosame@gmail.com>
2025-11-10 12:49:13 +0100

    feat: Add CategoryController and trust proxies

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -2,10 +2,8 @@
 
 namespace App\Http\Middleware;
 
-use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-
 class TrustProxies extends Middleware
 {
     /**
@@ -17,14 +15,13 @@ class TrustProxies extends Middleware
 
     /**
      * The headers that should be used to detect proxies.
+     *
+     * @var int
      */
-    public function handle(Request $request, Closure $next)
-    {
-        $this->proxies = '*';
-
-        $this->headers = Request::HEADER_X_FORWARDED_FOR | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_AWS_ELB;
-
-        return $next($request);
-    }
+    protected $headers =
+        Request::HEADER_X_FORWARDED_FOR |
+        Request::HEADER_X_FORWARDED_HOST |
+        Request::HEADER_X_FORWARDED_PORT |
+        Request::HEADER_X_FORWARDED_PROTO |
+        Request::HEADER_X_FORWARDED_AWS_ELB;
 }
-
