d6503e587c41a9d0514016a82adb54a0f1bf0049
tonyifeosame <tonyifeosame@gmail.com>
2025-11-11 16:33:58 +0100

    Fix session configuration and add CSRF meta tag

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -4,14 +4,15 @@ namespace App\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Middleware\TrustProxies as Middleware;
-class TrustProxies extends Middleware
+
+return new class extends Middleware
 {
     /**
      * The trusted proxies for this application.
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = '*';
+      protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
@@ -24,4 +25,4 @@ class TrustProxies extends Middleware
         Request::HEADER_X_FORWARDED_PORT |
         Request::HEADER_X_FORWARDED_PROTO |
         Request::HEADER_X_FORWARDED_AWS_ELB;
-}
+};
